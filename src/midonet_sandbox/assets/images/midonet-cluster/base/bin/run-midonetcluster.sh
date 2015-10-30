295dcb5474d4fea6f6aea01d219d9fa7ad5ae23d
Xavi León <xavi.leon@midokura.com>
2015-10-30 16:06:22 +0100

    Images: Configure cluster to use keystone
    Add support in cluster to authenticate against keystone if a keystone
    service is linked in the flavours file. In addition, added support also
    for liberty in the OSt components (only keystone currently).
    
    Change-Id: I20a4d9499fcc313aa90020efb931b52cfb2be2bf
    Signed-off-by: Xavi León <xavi.leon@midokura.com>

--- src/midonet_sandbox/assets/images/midonet-cluster/base/bin/run-midonetcluster.sh
+++ src/midonet_sandbox/assets/images/midonet-cluster/base/bin/run-midonetcluster.sh
@@ -6,6 +6,11 @@ if [[ `env | grep _PORT_2181_TCP_ADDR` ]]; then
     ZK_HOSTS="$(echo $ZK_HOSTS | sed 's/ /,/g')"
 fi
 
+# Get the linked keystone host
+if [[ `env | grep _PORT_35357_TCP_ADDR` ]]; then
+    KEYSTONE_HOST=$(env | grep _PORT_35357_TCP_ADDR | cut -d'=' -f2 | sort -u)
+fi
+
 IP=${LISTEN_ADDRESS:-`hostname --ip-address`}
 
 if [ -z "$ZK_HOSTS" ]; then
@@ -22,6 +27,27 @@ sed -i -e 's/zookeeper_hosts = .*$/zookeeper_hosts = '"$ZK_HOSTS"'/' $MIDO_CFG/$
 
 # Update cluster logs to report DEBUG
 sed -i 's/<root level="INFO">/<root level="DEBUG">/' /etc/midonet-cluster/logback.xml
+
+echo "Setting up basic conf for cluster..."
+mn-conf set -t default <<EOF
+zookeeper.zookeeper_hosts="$MIDO_ZOOKEEPER_HOSTS"
+EOF
+
+if [ -n "$KEYSTONE_HOST" ]; then
+    echo "Setting up keystone because a keystone container was linked"
+    mn-conf set -t default <<EOF
+    cluster.auth.provider_class="org.midonet.cluster.auth.keystone.v2_0.KeystoneService"
+    cluster.auth.admin_role="admin"
+    cluster.auth.keystone.tenant_name="admin"
+    cluster.auth.keystone.admin_token="ADMIN"
+    cluster.auth.keystone.host=$KEYSTONE_HOST
+    cluster.auth.keystone.port=35357
+EOF
+else
+    echo "Using MockAuth provider instead of keystone as no container was linked."
+fi
+
+echo "Starting cluster!"
 
 # Run cluster
 exec /sbin/init
